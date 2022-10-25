fb45ed56c754bbb198c95264c9d4e2d921971528
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-25 18:49:41 +0300

    hw9-js-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', start);
+refs.stopButton.addEventListener('click', stop);
+
+const randomIntegerFromInterval = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBackground(color) {
+  refs.bodyColor.style.backgroundColor = color;
+};
+
+let intervalColorChange = undefined;
+
+function start() {
+  intervalColorChange = setInterval(randomColor => {
+    randomColor = randomIntegerFromInterval();
+    changeBackground(randomColor);
+  }, 1000);
+  refs.stopButton.removeAttribute('disabled');
+  refs.startButton.setAttribute('disabled', true);
+};
+
+function stop() {
+  clearInterval(intervalColorChange);
+  refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', true);
+};
