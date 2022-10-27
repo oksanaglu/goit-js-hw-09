da2b54dde62c6f6c4e62cd3be956ff7b994e9caa
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-27 21:27:48 +0300

    hw9-js-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,51 @@
+import "flatpickr/dist/flatpickr.min.css";
+
+// const start = document.querySelector('[data-start]');
+// const days = document.querySelector('[data-days]');
+// const hours = document.querySelector('[data-hours]');
+// const minutes = document.querySelector('[data-minutes]');
+// const seconds = document.querySelector('[data-seconds]');
+
+let getEl = selector => document.querySelector(selector);
+// getEl('[data-start]').addEventListener('click', start);
+// getEl('[data-days]').addEventListener('click', selectedDays);
+// getEl('[data-hours]')
+// getEl('[data-minutes]')
+// getEl('[data-seconds]')
+
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+
+
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
