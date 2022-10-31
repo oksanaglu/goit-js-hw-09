5a47b3c04cca436030be15a751da12a51057db4f
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-31 20:48:41 +0200

    hw9-js-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,51 +1,77 @@
+
+import flatpickr from "flatpickr";
+
 import "flatpickr/dist/flatpickr.min.css";
 
-// const start = document.querySelector('[data-start]');
-// const days = document.querySelector('[data-days]');
-// const hours = document.querySelector('[data-hours]');
-// const minutes = document.querySelector('[data-minutes]');
-// const seconds = document.querySelector('[data-seconds]');
+const startTimerBtn = document.querySelector("button[data-start]");
+startTimerBtn.disabled = true;
 
-let getEl = selector => document.querySelector(selector);
-// getEl('[data-start]').addEventListener('click', start);
-// getEl('[data-days]').addEventListener('click', selectedDays);
-// getEl('[data-hours]')
-// getEl('[data-minutes]')
-// getEl('[data-seconds]')
+const timePicker = document.querySelector("#datetime-picker");
 
+const timerValue = {
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]")
+};
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    console.log(new Date())
 
+    if (selectedDates[0] < new Date()) {
+      startTimerBtn.disabled = true;
+      window.alert("Please choose a date in the future")
+    } else {
+      startTimerBtn.disabled = false
+      startTimerBtn.addEventListener("click", () => { changeTimerValue(selectedDates[0]) })
+    }
+  },
+};
 
+flatpickr(timePicker, options);
 
+function changeTimerValue() {
+  let timerId = setInterval(() => {
+    let countdown = new Date(timePicker.value) - new Date();
+    startTimerBtn.disabled = true;
+    timePicker.disabled = true;
+   
+    if (countdown >= 0) {
+      let timerData = convertMs(countdown);
+        timerValue.days.textContent = timerData.days;
+        timerValue.hours.textContent = timerData.hours;
+        timerValue.minutes.textContent = timerData.minutes;
+        timerValue.seconds.textContent = timerData.seconds;
+    } else {
+      clearInterval(timerId);
+    }
+  }, 1000);
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-//   return { days, hours, minutes, seconds };
-// }
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+    return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// const refs = {
+//   delay: document.querySelector('.delay'),
+//   step: document.querySelector('.step'),
+//   amount: document.querySelector('.amount'),
+// };
+
+
+
+
+// function createPromise(position, delay) {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     resolve({ position, delay });
+//   } else {
+//     reject({position, start})
+//   } 
+//     }, timeout);
+//   }
+//   )
+ 
+// }
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
